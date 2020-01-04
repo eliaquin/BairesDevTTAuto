72f35c39b33c2e393e72fb1528e321646ac4f41d
Eliaquín Encarnación <eliaquin@gmail.com>
2020-01-04 10:42:27 -0400

    Added more

--- BairesDevTTAuto/Core.cs
+++ BairesDevTTAuto/Core.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -18,8 +19,37 @@ namespace BairesDevTTAuto
             _driver.Navigate().GoToUrl("https://timetracker.bairesdev.com/");
             _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_UserNameTextBox")).SendKeys("eliaquin.encarnacion");
             _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_PasswordTextBox")).SendKeys("K1r4m1h$E1976");
-            var button = _driver.FindElement(By.ClassName("btn-blue"));
+            var button = _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_LoginButton"));
             button.Click();
+
+            const int counter = 0;
+            Sleep(counter, "BairesDev :: TimeTracker");
+
+            if (_driver.Title != "BairesDev :: TimeTracker")
+            {
+                Console.WriteLine("Unable to continue any further. Please, check your script.");
+                return;
+            }
+
+            var btn = _driver.FindElement(By.ClassName("btn-blue"));
+            btn.Click();
+            Thread.Sleep(5000);
+            _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_idProyectoDropDownList")).SendKeys("Indus");
+
+            Console.WriteLine("Yatta");
+        }
+
+        private void Sleep(int counter, string title)
+        {
+            while (_driver.Title != title)
+            {
+                Thread.Sleep(1000);
+                counter += 1;
+                if (counter > 5)
+                {
+                    break;
+                }
+            }
         }
 
         public void Dispose()
