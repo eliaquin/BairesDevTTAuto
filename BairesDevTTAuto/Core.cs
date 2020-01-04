b053c134adb4e7ec4fe2d95705c4cd81a82eea83
Eliaquín Encarnación <eliaquin@gmail.com>
2020-01-03 23:36:46 -0400

    Add project files.

---
+++ BairesDevTTAuto/Core.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BairesDevTTAuto
+{
+    internal class Core : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public Core()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Go()
+        {
+            _driver.Navigate().GoToUrl("https://timetracker.bairesdev.com/");
+            _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_UserNameTextBox")).SendKeys("eliaquin.encarnacion");
+            _driver.FindElement(By.Id("ctl00_ContentPlaceHolder_PasswordTextBox")).SendKeys("K1r4m1h$E1976");
+            var button = _driver.FindElement(By.ClassName("btn-blue"));
+            button.Click();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
---
+++ BairesDevTTAuto/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BairesDevTTAuto 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var auto = new Core();
+            auto.Go();
+        }
+    }
+}
